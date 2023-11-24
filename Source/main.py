a3a7a2d18e0fd664a1601d8cde12f4569ce8b335
qingxiawu <thanhhiepvos@gmail.com>
2023-11-24 20:09:15 +0700

    general

--- Source/main.py
+++ Source/main.py
@@ -1,10 +1,30 @@
-import sending_email
+#import sending_email
 import viewing_email
+
+def readFileConfig():
+    fileConfig = "config.txt"
+
+    username, email, password, host, SMTP, POP3, Autoload = "", "", "", "", "", "", ""
+    with open(fileConfig, "r") as file:
+        lines = file.read()
+        lines = lines.split('\n') # convert to list
+        
+        username = lines[1].split(': ')[1].split('<')[0].strip()
+        email = lines[1].split('<')[1].split('>')[0]
+        password = lines[2].split(': ')[1]
+        host = lines[3].split(': ')[1]
+        SMTP = lines[4].split(': ')[1]
+        POP3 = lines[5].split(': ')[1]
+        Autoload = lines[6].split(': ')[1]
+
+        file.close()
+
+    return username, email, password, host, SMTP, POP3, Autoload
 
 # Print Mail Client on Console
 def main():
-    print("Hello World")
-    print("test mahesvara7061's branch")
+    username, email, password, host, SMTP, POP3, Autoload = readFileConfig()
+    #print(username, email, password, host, SMTP, POP3, Autoload)
 
 if __name__ == "__main__":
     main()
