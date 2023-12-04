3392e4cefaa8315c304abc2580d6a1adfabff594
9tomissu <htdat1908@gmail.com>
2023-12-04 16:35:09 +0700

    nothing to do

---
+++ Source/download_email.py
@@ -0,0 +1,139 @@
+import os
+import socket
+import logging
+
+class ClientConfig:
+    def __init__(self, email, mailserver, pop3, filters):
+        self.email = email
+        self.mailserver = mailserver
+        self.pop3 = pop3
+        self.filters = filters
+
+logging.basicConfig(level=logging.ERROR)
+
+class EmailFilter:
+    def __init__(self, folder, flags):
+        self.folder = folder
+        self.flags = flags
+
+class EmailDownloader:
+    def __init__(self, client_config):
+        self.client_config = client_config
+        self.state_filename = "download_state.txt"
+        self.downloaded_emails = self.load_state()
+
+    def save_state(self, state):
+        with open(self.state_filename, "w") as state_file:
+            state_file.write(",".join(map(str, state)))
+
+    def load_state(self):
+        if os.path.exists(self.state_filename):
+            with open(self.state_filename, "r") as state_file:
+                state_content = state_file.read().strip()
+
+                if state_content:
+                    return set(map(int, state_content.split(',')))
+                else:
+                    return set()
+        else:
+            return set()
+
+    def should_save_to_folder(self, email_content, flags):
+        for flag in flags:
+            if flag in email_content:
+                return True
+        return False
+
+    def save_mail(self, msg_num, email_content):
+        email_content = email_content.replace("\r\n", "\n")
+
+        mailbox_path = "./Mailbox/"
+        if not os.path.exists(mailbox_path):
+            os.makedirs(mailbox_path)
+
+        saved = False
+
+        # Phân loại email và tải về các folder cụ thể
+        for filter in self.client_config.filters:
+            if self.should_save_to_folder(email_content, filter.flags):
+                filter_folder = os.path.join(mailbox_path, filter.folder)
+                if not os.path.exists(filter_folder):
+                    os.makedirs(filter_folder)
+
+                filename = f"{filter_folder}/{msg_num}.msg"
+                with open(filename, "w") as file:
+                    file.write(email_content)
+                
+                saved = True
+        emails = []
+
+        if not saved:
+            filter_folder = os.path.join(mailbox_path, './Inbox')
+            if not os.path.exists(filter_folder):
+                os.makedirs(filter_folder)
+            filename = f"{filter_folder}/{msg_num}.msg"
+            with open(filename, "w") as file:
+                file.write(email_content)
+            
+    def download_emails(self):
+        try:
+            # Mở socket
+            mail_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (self.client_config.mailserver, self.client_config.pop3)
+            mail_socket.connect(server_address)
+
+            # Nhận câu trả lời đầu tiên
+            response_user = mail_socket.recv(1024).decode()
+
+            # Gửi lệnh USER
+            mail_socket.send(f"USER {self.client_config.email}\r\n".encode())
+            response_user = mail_socket.recv(1024).decode()
+
+            # Gửi lệnh LIST
+            mail_socket.send(f"LIST\r\n".encode())
+            response_list = mail_socket.recv(1024).decode()
+            # print(f"List: {response_list}")
+
+            # Lấy danh sách các email
+            messages = response_list.split("\r\n")[1:-2]
+
+            # Lưu từng email
+            for message in messages:
+                msg_num = int(message.split()[0])
+                
+                # Kiểm tra lại trạng thái tải của email
+                # Nếu email chưa được tải thì bắt đầu tải về
+                if msg_num not in self.downloaded_emails:
+                    mail_socket.send(f"RETR {msg_num}\r\n".encode())
+
+                    email_content = ""
+                    while True:
+                        data = mail_socket.recv(1024).decode()
+
+                        if not data:
+                            break
+                    
+                        email_content += data
+
+                        if email_content.endswith('\r\n.\r\n'):
+                            break
+
+                    # Loại bỏ dòng đầu tiên "+OK ..."
+                    to_remove = email_content.split("\r\n")[0] 
+                    email_content = data.removeprefix(to_remove + "\r\n")
+                    
+                    # loại bỏ dòng cuối cùng
+                    email_content = email_content.removesuffix('\r\n.\r\n')
+                    
+                    self.save_mail(msg_num, email_content)
+                    
+                    self.downloaded_emails.add(msg_num)
+                    self.save_state(self.downloaded_emails)
+
+        except socket.error as se:
+            logging.error(f"Socket Error: {se}")
+        except IOError as ioe:
+            logging.error(f"I/O Error: {ioe}")
+        except Exception as e:
+            logging.error(f"Error: {e}")
+             
--- Source/viewing_email.py
+++ Source/viewing_email.py
@@ -1,263 +1,173 @@
-# Viewing Email
-import email
-from email import policy
-from email.parser import BytesParser
-from email.header import decode_header
-
-import socket
-import mimetypes
 import os
-import re
+import email
+from email.parser import BytesParser
+from email import policy
+from email.header import decode_header
+import socket
 
-# Biến toàn cục để lưu trạng thái
-downloaded_emails = set()
-emails = []
+from download_email import ClientConfig 
+from download_email import EmailFilter
+from download_email import EmailDownloader
 
-# Tạo socket
-def create_mail_socket():
-    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class EmailReader:
+    def __init__(self, client_config):
+        self.client_config = client_config
+        self.state_filename = "read_state.txt"
+        self.read_emails = self.load_state()
 
-#Kết nối đến server
-def connect_to_server(mail_socket, pop_server, port):
-    try:
-        mail_socket.connect((pop_server, port))
-        response = mail_socket.recv(1024).decode()
-        
-        if response.startswith('+OK'):
-            print(f"Setsuzoku ni seiko shi mashi ta. Server {pop_server}:{port}")
+    def save_state(self, state):
+        with open(self.state_filename, "w") as state_file:
+            state_file.write(",".join(map(str, state)))
+
+    def load_state(self):
+        if os.path.exists(self.state_filename):
+            with open(self.state_filename, "r") as state_file:
+                state_content = state_file.read().strip()
+
+                if state_content:
+                    return set(map(int, state_content.split(',')))
+                else:
+                    return set()
         else:
-            print(f"Kết nối thất bại. Server không phản hồi {response}")
-
-    except Exception as e:
-        print(f"Không thể kết nối đến server {e}")
-
-# Ngắt kết nối server
-def close_connection(mail_socket):
-    try:
-        mail_socket.send("QUIT\r\n".encode())
-        response_quit = mail_socket.recv(1024).decode()
-
-    except Exception as e:
-        print(f"Không thể ngắt kết nối server. {e}")
-    else:
-        print("Server ga setsudan sa re mashi ta .Go aisatsu 、 mata o ai shi masho u . <3")
+            return set()
         
-    finally:
-        mail_socket.close()
-
-# Lưu State
-def save_state(state):
-    with open("email_state.txt", "w") as state_file:
-        state_file.write(",".join(map(str, state)))
-
-# Tải State
-def load_state():
-    if os.path.exists("email_state.txt"):
-        with open("email_state.txt", "r") as state_file:
-            state_content = state_file.read().strip()
-
-            if state_content:
-                return set(map(int, state_content.split(',')))
-            else:
-                return set()
-    else:
-        return set()
-
-# Tải nội dung email
-def download_emails(mail_socket, username):
-    global downloaded_mails
-    global mails
-    mail_socket.send(f"USER {username}\r\n".encode())
-    response_user = mail_socket.recv(1024).decode()
-
-    downloaded_emails = load_state()
-
-    mail_socket.send("LIST\r\n".encode())
-    response_list = mail_socket.recv(1024).decode()
-    print(f"List: {response_list}")
-
-    messages = response_list.split("\r\n")[1:-2]
-
-    for message in messages:
-        msg_num = int(message.split()[0])
-
-        if msg_num not in downloaded_emails:
-            mail_socket.send(f"RETR {msg_num}\r\n".encode())
-
-            # Start with an empty string to store the email content
-            email_content = ""
-
-            # Loop until the end of the email content is reached
-            while True:
-                data = mail_socket.recv(1024).decode()
-                if not data:
-                    break
-                email_content += data
-
-                # Check for the end of the email content
-                if email_content.endswith('\r\n.\r\n'):
-                    break
-
-            # Đánh dấu là đã tải
-            downloaded_emails.add(msg_num) 
-
-            # Thêm thông tin vào danh sách emails
-            # sender, subject, main_content = extract_email_info(email_content)
-            # is_read = False
-            # emails.append((sender, subject, main_content, is_read))
-
-    # Lưu trạng thái sau khi tải xong
-    save_state(downloaded_emails)
-    
-# Hàm đọc email
-def read_email(mail_socket, username, email_number):
-    # Thực hiện lệnh USER
-    mail_socket.send(f"USER {username}\r\n".encode())
-    response_user = mail_socket.recv(1024).decode()
-
-    # Kiểm tra trạng thái lệnh PASS
-    if response_user.startswith('+OK'):
-        # Nếu đăng nhập thành công, thực hiện lệnh RETR để lấy nội dung của email
-        mail_socket.send(f"RETR {email_number}\r\n".encode())
-        
-        # Loop to receive the entire email content
-        response_retr = ""
-        while True:
-            data = mail_socket.recv(1024).decode()
-            if not data:
-                break
-            response_retr += data
+    def read_email(self, email_number):
+        try:
+            # Mở socket
+            mail_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (self.client_config.mailserver, self.client_config.pop3)
+            mail_socket.connect(server_address)
             
-            # Kiểm tra xem tới cuối nội dung email chưa
-            if response_retr.endswith('\r\n.\r\n'):
-                break
+            # Nhận câu trả lời đầu tiên
+            response = mail_socket.recv(1024).decode()
 
-        # print(f"After RETR {email_number} command: {response_retr}")
-        # In ra nội dung của email
-        print(f"Nội dung email:\n{response_retr}")
-        
-        # Đánh dấu email đã đọc
-        # emails[email_number - 1] = (emails[email_number - 1][0], emails[email_number - 1][1], emails[email_number - 1][2], True)
+            # Gửi lệnh USER
+            mail_socket.send(f"USER {self.client_config.email}\r\n".encode())
+            response_user = mail_socket.recv(1024).decode()
+            # print(response_user)
 
-        # Kiểm tra xem email có file đính kèm không
-        if "Content-Disposition: attachment" in response_retr:
-            attachment_start = response_retr.find("Content-Disposition: attachment")
-            save_attachment(response_retr, attachment_start, email_number)
-    else:
-        print("Đăng nhập thất bại. Hãy kiểm tra lại.")
+            # Nếu server trả lời "+OK"
+            if response_user.startswith('+OK'):
+                # Gửi lệnh RETR để lấy email cụ thể
+                mail_socket.send(f"RETR {email_number}\r\n".encode())
 
-def get_boundary(email_content, attachment_start):
-    # Find the boundary parameter in the Content-Type header
-    content_type_index = email_content.find("Content-Type", attachment_start)
-    boundary_start_index = email_content.find("boundary=", content_type_index)
-    boundary_end_index = email_content.find("\r\n", boundary_start_index)
-    
-    # Extract the boundary string
-    boundary = email_content[boundary_start_index + len("boundary="):boundary_end_index].strip()
-    
-    # Clean up the boundary string
-    boundary = boundary.replace('"', "")
-    
-    return boundary
+                response_retr = ""
+                while True:
+                    data = mail_socket.recv(1024).decode()
 
-# Hàm để đọc và lưu file đính kèm
-def save_attachment(email_content, attachment_start, email_number):
-    # Tìm vị trí kết thúc của dữ liệu đính kèm
-    attachment_end = email_content.find("\r\n\r\n", attachment_start)
-    attachment_data = email_content[attachment_end:].encode()
+                    if not data:
+                        break
 
-    # Lấy đối tượng email từ nội dung email
-    email_message = BytesParser(policy=policy.default).parsebytes(email_content.encode())
+                    response_retr += data
 
-    # Xác định loại nội dung của file đính kèm
-    content_type = email_message.get_content_type()
-    
-    # Lấy mô tả nếu có
-    content_disposition, params = email_message.get_content_disposition(), email_message.get_params()
+                    if response_retr.endswith('\r\n.\r\n'):
+                        break
 
-    # Giải mã tên file nếu có
-    if params:
-        filename_info = decode_header(params.get('filename', ''))
-        filename = filename_info[0][0] if filename_info[0][0] else "unknown_attachment"
-    else:
-        filename = "unknown_attachment"
+                # Loại bỏ dòng đầu tiên
+                to_remove = data.split("\r\n")[0] 
+                response_retr = response_retr.removeprefix(to_remove + "\r\n")
 
-    # Thư mục để lưu tệp đính kèm
-    attachment_directory = "C:/Users/htdat/Desktop/Source/VS_code/Python/Socket Project/Attachments"
-
-    # Kiểm tra xem thư mục tồn tại chưa, nếu chưa thì tạo mới
-    if not os.path.exists(attachment_directory):
-        os.makedirs(attachment_directory)
-
-    # Xác định đường dẫn lưu file đính kèm
-    attachment_file_path = os.path.join(attachment_directory, f"attachment_{email_number}_{filename}")
-
-    # Lưu dữ liệu file đính kèm xuAống máy cục bộ của client
-    with open(attachment_file_path, "wb") as attachment_file:
-        attachment_file.write(attachment_data)
-
-    print(f"Đã lưu file đính kèm của Email {email_number} vào đường dẫn: {attachment_file_path}")
-
-
-
-def extract_email_info(email_content):
-    # Phân tích nội dung email
-    msg = email.message_from_string(email_content)
-
-    # Trích xuất người gửi
-    sender = msg.get('From')
-
-    # Trích xuất chủ đề
-    subject = decode_header(msg.get('Subject'))[0][0]
-    if isinstance(subject, bytes):
-        # Nếu chủ đề là bytes, hãy chuyển đổi nó thành chuỗi
-        subject = subject.decode()
-
-    # Trích xuất nội dung chính
-    if msg.is_multipart():
-        # Nếu email có nhiều phần, hãy lấy phần đầu tiên
-        main_content = msg.get_payload()[0].get_payload()
-    else:
-        # Nếu không, chỉ cần lấy nội dung
-        main_content = msg.get_payload()
-
-    return sender, subject, main_content
-
-def print_list(emails):
-    print("Đây là danh sách email trong mailbox của bạn: ")
-    
-    if not emails:
-        print("Trống")
-    else:
-        number = 1
-        for email in emails: 
-            is_read = ''
-            
-            if not email[3]:
-                is_read = "(chưa đọc)"
+                # loại bỏ dòng cuối cùng
+                response_retr = response_retr.removesuffix('\r\n.\r\n')
                 
-            print(f"{number}.{is_read} {email[0]} {email[1]}")
-            number += 1
+                # Xuất ra màn hình nội dung mail vừa lấy
+                print(f"Nội dung email:\n{response_retr}")
+
+                # Lưu file đính kèm nếu có
+                if "Content-Disposition: attachment" in response_retr:
+                    attachment_start = response_retr.find("Content-Disposition: attachment")
+                    self.save_attachment(response_retr, attachment_start, email_number)
+            else:
+                print("Đăng nhập thất bại. Hãy kiểm tra lại.")
+        except Exception as e:
+            print(f"Error while reading email: {e}")
+        finally:
+            # Đóng socket sau khi hoàn thành
+            mail_socket.close()
+
+    def save_attachment(self, email_content, attachment_start, email_number):
+        attachment_end = email_content.find("\r\n\r\n", attachment_start)
+        attachment_data = email_content[attachment_end:].encode()
+
+        email_message = BytesParser(policy=policy.default).parsebytes(email_content.encode())
+        content_type = email_message.get_content_type()
+
+        content_disposition, params = email_message.get_content_disposition(), email_message.get_params()
+
+        if params:
+            filename_info = decode_header(params.get('filename', ''))
+            filename = filename_info[0][0] if filename_info[0][0] else "unknown_attachment"
+        else:
+            filename = "unknown_attachment"
+
+        attachment_directory = os.path.join(".", "Attachments", self.client_config.email)
+
+        if not os.path.exists(attachment_directory):
+            os.makedirs(attachment_directory)
+
+        attachment_file_path = os.path.join(attachment_directory, f"attachment_{email_number}_{filename}")
+
+        with open(attachment_file_path, "wb") as attachment_file:
+            attachment_file.write(attachment_data)
+
+        print(f"Đã lưu file đính kèm của Email {email_number} vào đường dẫn: {attachment_file_path}")
+
+    def extract_email_info(self, email_content):
+        msg = email.message_from_string(email_content)
+        sender = msg.get('From')
+        subject = decode_header(msg.get('Subject'))[0][0]
+        if isinstance(subject, bytes):
+            subject = subject.decode()
+
+        if msg.is_multipart():
+            main_content = msg.get_payload()[0].get_payload()
+        else:
+            main_content = msg.get_payload()
+
+        return sender, subject, main_content
+
+    def mark_email_as_read(self, email_number):
+        self.read_emails.add(email_number)
+        self.save_state(self.read_emails)
+        print(f"Email {email_number} marked as read.")
+        
+    def print_list(self):
+        print("DEBUG: Emails structure:", self.read_emails)
+
+        print("Đây là danh sách email trong mailbox của bạn: ")
+        if not self.read_emails:
+            print("Trống")
+        else:
+            number = 1
+            for email in self.read_emails:
+                print("DEBUG: Current email tuple:", email)
+
+                is_read = ''
+                if not email[3]:
+                    is_read = "(chưa đọc)"
+
+                print(f"{number}.{is_read} {email[0]} {email[1]}")
+                number += 1
+
+def main():
+    # Replace these values with your actual configurations
+    email_config = ClientConfig(
+        mailserver="127.0.0.1",
+        pop3=3335,
+        email="htdat222@clc.fitus.edu.vn",
+        filters=[EmailFilter("Folder1", ["flag1", "flag2"]), EmailFilter("Folder2", ["flag3"])]
+    )
+
+    # Example usage of EmailDownloader
+    downloader = EmailDownloader(email_config)
+    downloader.download_emails()
+
+    # Example usage of EmailReader
+    reader = EmailReader(email_config)
+    reader.print_list()
+
+    email_number = int(input("Enter the email number you want to read: "))
+    reader.read_email(email_number)
 
 if __name__ == "__main__":
-    # Tạo mail socket
-    mail_socket = create_mail_socket()
-    # Thông tin
-    pop_server = "127.0.0.1"
-    port = 3335
-    
-    username = "htdat222@clc.fitus.edu.vn"
-    # Kết nối đến server
-    connect_to_server(mail_socket, pop_server, port)
-    
-    # Tiến hành tải email
-    if mail_socket:
-        download_emails(mail_socket, username)     
-    
-    # print_list(emails)
-    
-    email_number = int(input("Dono meru wa yomi tai desu ka: "))
-    read_email(mail_socket, username, email_number)
-    
-    # Ngắt kết nối server
-    close_connection(mail_socket)
+    main()
