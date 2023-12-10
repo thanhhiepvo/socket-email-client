b49ce17605d81a37d7e825bd62973f783465e2ce
huynh dat <150450313+9tomissu@users.noreply.github.com>
2023-12-10 23:17:57 +0700

    ?

--- Source/getting_email.py
+++ Source/getting_email.py
@@ -15,17 +15,20 @@ import base64
 class EmailReader:
     def __init__(self, client_config):
         self.client_config = client_config
-        self.state_filename = "read_state.txt"
-        self.read_emails = self.load_state()
-        #self.file_name = None
+        self.state_folder = "State"
+        self.state_filename = f"{self.client_config.email}.txt"
+        self.state_filepath = os.path.join(self.state_folder, self.state_filename)
+        self.downloaded_emails = self.load_state()
+        if not os.path.exists(self.state_folder):
+            os.makedirs(self.state_folder)
 
     def save_state(self, state):
-        with open(self.state_filename, "w") as state_file:
+        with open(self.state_filepath, "w") as state_file:
             state_file.write(",".join(map(str, state)))
 
     def load_state(self):
-        if os.path.exists(self.state_filename):
-            with open(self.state_filename, "r") as state_file:
+        if os.path.exists(self.state_filepath):
+            with open(self.state_filepath, "r") as state_file:
                 state_content = state_file.read().strip()
 
                 if state_content:
@@ -46,10 +49,8 @@ class EmailReader:
         mailbox_path = "./Mailbox/" + self.client_config.email
         if not os.path.exists(mailbox_path):
             os.makedirs(mailbox_path)
-
         saved = False
-
-        # Phân loại email và tải về các folder cụ thể
+        
         for filter in self.client_config.filters:
             if self.should_save_to_folder(email_content, filter.flags):
                 filter_folder = os.path.join(mailbox_path, filter.folder)
@@ -58,10 +59,8 @@ class EmailReader:
 
                 filename = f"{filter_folder}/{subject}.msg"
                 with open(filename, "w") as file:
-                    file.write(email_content)
-                
+                    file.write(email_content)     
                 saved = True
-        emails = []
 
         if not saved:
             filter_folder = os.path.join(mailbox_path, './Inbox')
@@ -100,21 +99,13 @@ class EmailReader:
         
     def read_email(self, email_number):
         try:
-            # Mở socket
             mail_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_address = (self.client_config.mailserver, self.client_config.pop3)
             mail_socket.connect(server_address)
-            
-            # Nhận câu trả lời đầu tiên
-            response = mail_socket.recv(1024).decode()
-
-            # Gửi lệnh USER
             mail_socket.send(f"USER {self.client_config.email}\r\n".encode())
             response_user = mail_socket.recv(1024).decode()
 
-            # Nếu server trả lời "+OK"
             if response_user.startswith('+OK'):
-                # Gửi lệnh RETR để lấy email cụ thể
                 mail_socket.send(f"RETR {email_number}\r\n".encode())
 
                 response_retr = ""
@@ -129,14 +120,10 @@ class EmailReader:
                     if response_retr.endswith('\r\n.\r\n'):
                         break
 
-                # Loại bỏ dòng đầu tiên
                 to_remove = data.split("\r\n")[0] 
                 response_retr = response_retr.removeprefix(to_remove + "\r\n")
-
-                # loại bỏ dòng cuối cùng
                 response_retr = response_retr.removesuffix('\r\n.\r\n')
 
-                # Lưu file đính kèm nếu có
                 if "Content-Disposition: attachment" in response_retr:
                     file_start = response_retr.rfind("Content-Type:")
                     attachment = response_retr[file_start:]
@@ -155,7 +142,6 @@ class EmailReader:
         except Exception as e:
             print(f"Error while reading email: {e}")
         finally:
-            # Đóng socket sau khi hoàn thành
             mail_socket.close()
 
     def save_attachment(self, attachment, email_number, subject):
